8e74b6a601238ba78c84f96a37f1f9028a612bba
STROBEL JOSEPH <732012@ehs-s-232-26.local>
2016-12-14 10:52:51 -0600

    Initial Commit

---
+++ tableView/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  tableView
+//
+//  Created by STROBEL, JOSEPH on 12/14/16.
+//  Copyright © 2016 STROBEL, JOSEPH. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
