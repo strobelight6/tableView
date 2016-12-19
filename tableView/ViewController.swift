749b00f97c09ce0d5c023256da4599d4af63859a
STROBEL JOSEPH <732012@ehs-s-232-26>
2016-12-19 10:46:54 -0600

    first commit

--- tableView/ViewController.swift
+++ tableView/ViewController.swift
@@ -8,11 +8,42 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
 
+    let people = [
+        ("Adam Ford", "New York"),
+        ("Joe Strobel", "Alabama"),
+        ("Carter West", "Texas")
+    ]
+    
+    let videos = [
+    ("Basketball", "84 Videos"),
+    ("Soccer", "28 Videos"),
+    ("Swimming", "54 Videos"),
+    ("Football", "31 Videos"),
+    ("Baseball", "2 Videos")
+    ]
+    
+    //how many sections are in the view
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    //table view will return int for how many rows
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return people.count
+    }
+    
+    //what is in the table
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = UITableViewCell()
+        var (personName, personLocation) = people[indexPath.row]
+        cell.textLabel?.text = personName
+        return cell
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
     }
 
     override func didReceiveMemoryWarning() {
